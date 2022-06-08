6c83bd623042134d9095d71144c3795671a0b233
Veronika Bandaevskaya <nika020204@mail.ru>
2022-06-08 21:47:02 +0700

    Some menus

---
+++ Assets/Resources/Scripts/SettingsMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer mainMixer;
+    public void SetVolume(float volume)
+    {
+        mainMixer.SetFloat("Volume", volume);
+    }
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex, true);
+    }
+}
+
