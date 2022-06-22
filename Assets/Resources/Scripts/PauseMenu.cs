e664d8f2574f3759a697e72be88803b1423bc4de
Veronika Bandaevskaya <v.bandaevskaya@mail.ru>
2022-06-22 18:10:32 +0700

    pauseOnEsc

--- Assets/Resources/Scripts/PauseMenu.cs
+++ Assets/Resources/Scripts/PauseMenu.cs
@@ -6,17 +6,36 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
 	[SerializeField] GameObject pauseMenu;
+	bool GameIsPaused = false;
+	
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			if (GameIsPaused)
+			{
+				Resume();
+			}
+			else
+			{
+				Pause();
+			}
+
+		}
+	}
 
 	public void Pause()
 	{
 		pauseMenu.SetActive(true);
 		Time.timeScale = 0f;
+		GameIsPaused = true;
 	}
 
 	public void Resume()
 	{
 		pauseMenu.SetActive(false);
 		Time.timeScale = 1f;
+		GameIsPaused = false;
 	}
 
 	public void Home(int sceneID)
