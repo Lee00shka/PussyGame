713bf7cd412b1a7ed8c30a11fa107dd5157fd1cf
Veronika Bandaevskaya <v.bandaevskaya@mail.ru>
2022-06-22 15:49:08 +0700

    NewMovement

--- Assets/Resources/Scripts/PlayerController.cs
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -5,13 +5,28 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
-
+	
+	//Components
     private Rigidbody2D rb;
-    private Vector2 moveInput;
-    private Vector2 moveVelocity;
+
+	//Player
+	float walkSpeed = 4f;
+	float speedLimiter 0.7f;
+	float inputHorizontal;
+	float inputVertical;
+    //private Vector2 moveInput;
+    //private Vector2 moveVelocity;
     
     public static bool glasses = false;
     public static bool key = false;
+
+	//Animations and states
+	Animator animator;
+	string currentState;
+	const string PLAYER_WALK_LEFT = "Player_Walk_Left";
+	const string PLAYER_WALK_RIGHT = "Player_Walk_Right";
+	const string PLAYER_WALK_UP = "Player_Walk_Up";
+	const string PLAYER_WALK_DOWN = "Player_Walk_Down";
 
     void Attack()
     {
@@ -24,15 +39,45 @@ public class PlayerController : MonoBehaviour
     }
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
+		animator = gameObject.GetComponent<Animator>();
     }
     void Update()
     {
-        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        moveVelocity = moveInput.normalized * speed;
+		inputHorizontal = Input.GetAxisRaw("Horizontal");
+		inputVertical = Input.GetAxisRaw("Vertical");
+        //moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        //moveVelocity = moveInput.normalized * speed;
+
     }
     void FixedUpdate() 
     {
-        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+		if (inputHorizontal != 0 || inputVertical != 0)
+		{
+			if (inputHorizontal != 0 && inputVertical != 0)
+			{
+				inputHorizontal *= speedLimiter;
+				inputVertical *= speedLimiter;
+			}
+			rb.velocity = new Vector2(inputHorizontal * walkSpeed, inputVertical * walkSpeed);
+		}
+		else
+		{
+			rb.velocity = new Vector2(0f, 0f);
+		}
+        //rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
     }
+
+	//Animaton state changer
+	void ChangeAnimationState(string newState)
+	{
+		//Stop animation from interrupting itself
+		if (currentState == newState) return;
+		
+		//Play new animation
+		animator.Play(newState);
+
+		//Update currentState
+		currentState = newState;		
+	}
 }
