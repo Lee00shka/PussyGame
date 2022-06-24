3c37829a709fed32e908810e9ea0e58f62771529
Veronika Bandaevskaya <v.bandaevskaya@mail.ru>
2022-06-24 19:27:40 +0700

    oh

--- Assets/Resources/Scripts/ScoreManager.cs
+++ Assets/Resources/Scripts/ScoreManager.cs
@@ -2,16 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
     public static int numOfPussies;
     public static int numOfEnchanted = 0;
-    public TextMesh score;
+    private TextMeshPro score;
 
     private void Start()
     {
-        score = GetComponent<TextMesh>();
+        score = GetComponent<TextMeshPro>();
     }
     private void Update()
     {
